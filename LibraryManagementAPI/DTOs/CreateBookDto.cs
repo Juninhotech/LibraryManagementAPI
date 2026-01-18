75c5616c468cd75b1697398d67ed64f1ef31d0c2
Juninho <juninhopeter1@gmail.com>
2026-01-18 18:52:34 +0100

    Added DTos

---
+++ LibraryManagementAPI/DTOs/CreateBookDto.cs
@@ -0,0 +1,9 @@
+﻿namespace LibraryManagementAPI.DTOs
+{
+    public class CreateBookDto
+    {
+        public string Title { get; set; } = string.Empty;
+        public string Author { get; set; } = string.Empty;
+        public string ISBN { get; set; } = string.Empty;
+    }
+}
---
+++ LibraryManagementAPI/DTOs/UpdateBookDto.cs
@@ -0,0 +1,10 @@
+﻿namespace LibraryManagementAPI.DTOs
+{
+    public class UpdateBookDto
+    {
+        public string Title { get; set; } = string.Empty;
+        public string Author { get; set; } = string.Empty;
+        public string ISBN { get; set; } = string.Empty;
+        public DateTime PublishedDate { get; set; }
+    }
+}
